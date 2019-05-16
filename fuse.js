90be67af636af3e42ca32e96b78a1db85322c5fc
blomman <johnny@gumbler.com>
2019-05-16 09:26:14 +0200

    Fuse

--- fuse.js
+++ fuse.js
@@ -1,60 +1,56 @@
-const { FuseBox, CSSPlugin, SassPlugin, WebIndexPlugin, Sparky, BabelPlugin, QuantumPlugin } = require("fuse-box");
+const { FuseBox, CSSPlugin, SassPlugin, WebIndexPlugin, Sparky, BabelPlugin, QuantumPlugin } = require('fuse-box');
 const BUILD_PATH = 'build/';
 
-Sparky.task("default", ['clean', 'copy-assets', 'dev'], () => { });
-Sparky.task("prod", ['clean', 'copy-assets', 'production'], () => {});
+Sparky.task('default', ['clean', 'copy-assets', 'dev'], () => {});
+Sparky.task('prod', ['clean', 'copy-assets', 'production'], () => {});
 Sparky.task('clean', () => Sparky.src(BUILD_PATH).clean(BUILD_PATH));
-Sparky.task('copy-assets', () => Sparky.src("assets/**/**.*", { base: './src' }).dest('./build'));
+Sparky.task('copy-assets', () => Sparky.src('assets/**/**.*', { base: './src' }).dest('./build'));
 Sparky.task('dev', dev);
 Sparky.task('production', prod);
 
 function init(isProd) {
   return FuseBox.init({
-    homeDir: "src",
-    output: "build/$name.js",
+    homeDir: 'src',
+    output: 'build/$name.js',
     plugins: [
       BabelPlugin({
         config: {
           sourceMaps: true,
-          presets: ["es2015"],
-          plugins: [
-
-          ],
+          presets: ['es2015'],
+          plugins: [],
         },
       }),
       [SassPlugin(), CSSPlugin()],
       CSSPlugin(),
-      WebIndexPlugin({ template: 'src/index.html' })
-    ]
+      WebIndexPlugin({ template: 'src/index.html' }),
+    ],
   });
 }
 
 function dev() {
   const fuse = init(false);
   fuse.dev();
-  fuse.bundle("app")
+  fuse
+    .bundle('app')
     .watch()
-    .instructions("> index.js")
-  Sparky.task("clean", () => {
-    return Sparky.src("build").clean("build");
+    .instructions('> index.js');
+  Sparky.task('clean', () => {
+    return Sparky.src('build').clean('build');
   });
-  Sparky.task("watch:images", () => {
-    return Sparky.watch("**/*.+(svg|png|jpg|gif)", {base: "./src"})
-      .dest("./build");
+  Sparky.task('watch:images', () => {
+    return Sparky.watch('**/*.+(svg|png|jpg|gif)', { base: './src' }).dest('./build');
   });
   fuse.run();
 }
 
 function prod() {
   const fuse = init(true);
-  fuse.bundle("app")
-    .instructions("> index.js")
-  Sparky.task("clean", () => {
-    return Sparky.src("build").clean("build");
+  fuse.bundle('app').instructions('> index.js');
+  Sparky.task('clean', () => {
+    return Sparky.src('build').clean('build');
   });
-  Sparky.task("watch:images", () => {
-    return Sparky.watch("**/*.+(svg|png|jpg|gif)", {base: "./src"})
-      .dest("./build");
+  Sparky.task('watch:images', () => {
+    return Sparky.watch('**/*.+(svg|png|jpg|gif)', { base: './src' }).dest('./build');
   });
   fuse.run();
 }
